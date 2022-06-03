744ed6b6667103d5e0a14fde816293fefbf1e428
MohamadHelli <mou.hamad123@hotmail.com>
2022-06-03 18:01:46 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let days = JSON.parse(list);
+
+function updateHTML(arr) {
+    for (let info of arr) {
+        document.getElementById("result").innerHTML += `
+        <div class="col-lg-3 col-md-5 col-sm-10 col-xs-12">
+            <div class="card shadow-lg mb-5 bg-body rounded" style="width: 18rem;">
+            <div class="m-3 d-flex justify-content-between">
+                <div>
+                    <button type="button" class="btn btn-info text-light" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .6rem; --bs-btn-font-size: .75rem;">Task</button>
+                </div>
+                <div>
+                    <i class="fa-solid fa-bookmark"></i>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </div>
+            </div>
+                <img src="${info.image}" class="card-img-top img-thumbnail">
+            <div class="card-body">
+                <h5 class="card-title text-center">${info.title}</h5>
+                <p>${info.description}</p>
+                <hr>
+                <p class="card-text"> 
+                <i class="fa-solid fa-triangle-exclamation"></i> Priority level: <a class="btn-level btn btn-success btn-sm"><span class="result">${info.level}</span></a></p>
+                <p><i class="fa-solid fa-calendar-days"></i> Deadline: ${info.Date}</p>
+                <hr>
+                <div class="d-flex d-flex justify-content-end gap-2">
+                    <a href=" " class="btn btn-danger"> <i class="fa-solid fa-trash-can"></i> Delete</a>
+                    <a href=" " class="btn btn-success"> <i class="fa-solid fa-circle-check"></i> Done</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    }
+}
+
+function likeAction() {
+    let btns = document.getElementsByClassName("btn-level");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            days[i].level++;
+            document.getElementsByClassName("result")[i].innerHTML = days[i].level;
+        });
+    }
+}
+updateHTML(days);
+likeAction();
